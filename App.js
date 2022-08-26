4333b416fcf5e9debb8882b27a12f321f24d8f0e
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2022-08-26 11:44:23 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+      Richard <code>first app</code> web
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      
+      
+        <h1 style={{color:"blue"}}>4 Tips to master self discipline</h1>
+        <p>Having a strong sense of self-discipline is a game-changer in life. No matter what your goals are, who you are, or what you do—one can always benefit from having a firmly rooted self-discipline to draw upon.</p>
+        <p>It can move you in times where it becomes difficult to move at all and help to drive you forward, away from stagnancy. Cultivating a set of self-discipline practices and skills? Well, it takes a balance of a  little extra intention, a consciousness of what you’re doing, and a hefty serving of will power. Read, digest, and implement these steps to make self-discipline one less worry on the agenda.</p>
+        <h2>1. Be Honest with Yourself</h2>
+        <p>At the heart of having a drive for self-discipline must be one’s ability to cut through the layers of fog that life can bring. Try your best to work through and challenge perceptions, observing your life through a series of facts.</p>
+        <p>Remind yourself what constitutes a “fact,” and pull away from the emotion. Emotion has a time and a place, and it should never be buried. Though building up self-discipline can work quite nicely if you develop the skill of viewing your reality in a “matter of fact” manner that allows you to recognize and accept what truly “is true.”</p>
+        <h2>2.Know the weakness</h2>
+        <p>At the heart of having a drive for self-discipline must be one’s ability to cut through the layers of fog that life can bring. Try your best to work through and challenge perceptions, observing your life through a series of facts.</p>
+        <p>Remind yourself what constitutes a “fact,” and pull away from the emotion. Emotion has a time and a place, and it should never be buried. Though building up self-discipline can work quite nicely if you develop the skill of viewing your reality in a “matter of fact” manner that allows you to recognize and accept what truly “is true.”</p>
+        <h2>3.Forget the goal system</h2>
+        <p>At the heart of having a drive for self-discipline must be one’s ability to cut through the layers of fog that life can bring. Try your best to work through and challenge perceptions, observing your life through a series of facts.</p>
+        <p>Remind yourself what constitutes a “fact,” and pull away from the emotion. Emotion has a time and a place, and it should never be buried. Though building up self-discipline can work quite nicely if you develop the skill of viewing your reality in a “matter of fact” manner that allows you to recognize and accept what truly “is true.”</p>
+        <h2>4. View Change as opportunity</h2>
+        <p>At the heart of having a drive for self-discipline must be one’s ability to cut through the layers of fog that life can bring. Try your best to work through and challenge perceptions, observing your life through a series of facts.</p>
+        <p>Remind yourself what constitutes a “fact,” and pull away from the emotion. Emotion has a time and a place, and it should never be buried. Though building up self-discipline can work quite nicely if you develop the skill of viewing your reality in a “matter of fact” manner that allows you to recognize and accept what truly “is true.”</p>
+        <h1>Conclusion</h1>
+
+       
+        
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
